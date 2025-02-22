7503c14d7bdb37f6a92c982ceb87e4d6dabea97a
Emil <e.motroen@gmail.com>
2025-02-22 18:55:42 +0100

    Test script on branch

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import datetime
 
 def print_current_time():
     print("Current time:", datetime.datetime.now())
-    print('Berlin')
+    print('New York')
 scheduler = BlockingScheduler(timezone=utc, job_defaults={'misfire_grace_time': 10})
 scheduler.add_job(print_current_time, 'interval', minutes=1)
 
