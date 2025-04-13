0d812e8bdc1cde8923647e16650eb00143fd66b9
Emil <e.motroen@gmail.com>
2025-04-13 14:55:59 +0200

    change city

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import datetime
 
 def print_current_time():
     print("Current time:", datetime.datetime.now())
-    print('Kyoto from main branch')
+    print('Mumbai from main branch with test being successful')
 
 scheduler = BlockingScheduler(timezone=utc, job_defaults={'misfire_grace_time': 10})
 scheduler.add_job(print_current_time, 'interval', minutes=1)
